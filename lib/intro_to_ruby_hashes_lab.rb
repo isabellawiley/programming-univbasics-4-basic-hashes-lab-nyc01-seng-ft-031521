fb7c2407f14981de2ee0fdba551db6954a397a7e
Isabella Wiley <wileyisabella@gmail.com>
2021-03-09 05:06:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,14 +1,19 @@
 def new_hash
   # return an empty hash
-  my_hash = Hash.new
+  my_empty_hash = Hash.new
+  return my_empty_hash
 end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  hash_brown = {
+    :message => "Hey!"
+  }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  
 end
 
 
